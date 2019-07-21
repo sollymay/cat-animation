da1852ca600752b577ba1ca89a2546e36e2e51a8
Salomon May <sollymay@gmail.com>
2019-07-21 00:28:51 -0400

    created cat animation in box

---
+++ animation/lib/src/widgets/cat.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Cat extends StatelessWidget {
+
+  Widget build(BuildContext context) {
+    return Image.network(
+      'https://i.imgur.com/QwhZRyL.png',
+    );
+  }
+}
